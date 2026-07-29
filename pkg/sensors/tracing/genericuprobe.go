amarkdotdev/tetragon

commit b8a63e96c91e24baedc6720c7e15b003b98c006f

uprobe: remove os.ErrClosed check
Simplify the code by ensuring that each opened file is only attempted to
be closed once.

Signed-off-by: Andy Strohman <astrohma@isovalent.com>

pkg/sensors/tracing/genericuprobe.go
@@ -628,7 +628,7 @@ func cleanupUprobeEntries(ids []idtable.EntryID, openedFiles []*os.File) error {
 	}
 
 	for _, entryFile := range openedFiles {
-		if err := entryFile.Close(); err != nil && !errors.Is(err, os.ErrClosed) {
+		if err := entryFile.Close(); err != nil {
 			errs = errors.Join(errs, fmt.Errorf("problem closing path %q: %w", entryFile.Name(), err))
 		}
 	}
@@ -699,18 +699,13 @@ func createGenericUprobeSensor(
 
 		var entryFile *os.File
 
-		ids, err = addUprobe(&uprobe, entryFile, ids, &in, &has)
-		if err != nil {
-			if entryFile != nil {
-				if closeErr := entryFile.Close(); closeErr != nil && !errors.Is(closeErr, os.ErrClosed) {
-					err = errors.Join(err, closeErr)
-				}
-			}
-			return nil, err
-		}
-
 		if entryFile != nil {
 			openedFiles = append(openedFiles, entryFile)
+		}
+
+		ids, err = addUprobe(&uprobe, entryFile, ids, &in, &has)
+		if err != nil {
+			return nil, err
 		}
 	}
 
@@ -746,7 +741,7 @@ func createGenericUprobeSensor(
 		PostLoadHook: func() error {
 			var errs error
 			for _, entryFile := range openedFiles {
-				if err = entryFile.Close(); err != nil && !errors.Is(err, os.ErrClosed) {
+				if err = entryFile.Close(); err != nil {
 					errs = errors.Join(errs, fmt.Errorf("problem closing path %q: %w", entryFile.Name(), err))
 				}
 			}

commit e15c34627744c18b344e8728c181023ea0fbed47

uprobe: fix issue when deleting policy
Prior to this change, when a policy that utilizes the binaryDigests
configuration is deleted, we would get an error message like:

level=warn msg="Destroy hook failed" sensor=generic_uprobe error="problem closing path \"/home/andy/src/sleep\": close /home/andy/src/sleep: file already closed"

This happens because the file was closed after the sensor was loaded. So
when we try to close the file again when the sensor is destroyed, we hit
this error.

We need to attempt to close the file during destroy for the case where
the sensor is created, but never loaded, and then destroyed. This could
happen when a policy's sensors are generated, and a subsequent sensor
fails to be created after the uprobe sensor was created.

So, add an additional parameter to cleanupUprobeEntries to signify that
a file already being closed is OK, and utilize this for the destroy
callback.

fixes: b8a63e96c91e ("uprobe: remove os.ErrClosed check")
Signed-off-by: Andy Strohman <astrohma@isovalent.com>

pkg/sensors/tracing/genericuprobe.go
@@ -699,7 +699,7 @@ func initUprobeSelectors(spec *v1alpha1.UProbeSpec, in *addUprobeIn, state *upro
 	return nil
 }
 
-func cleanupUprobeEntries(ids []idtable.EntryID, openedFiles map[string]*os.File) error {
+func cleanupUprobeEntries(ids []idtable.EntryID, openedFiles map[string]*os.File, alreadyClosedOk bool) error {
 	var errs error
 
 	for _, id := range ids {
@@ -724,6 +724,9 @@ func cleanupUprobeEntries(ids []idtable.EntryID, openedFiles map[string]*os.File
 
 	for path, entryFile := range openedFiles {
 		if err := entryFile.Close(); err != nil {
+			if alreadyClosedOk && errors.Is(err, os.ErrClosed) {
+				continue
+			}
 			errs = errors.Join(errs, fmt.Errorf("problem closing path %q: %w", path, err))
 		}
 	}
@@ -907,7 +910,7 @@ func createGenericUprobeSensor(
 
 	defer func() {
 		if retErr != nil {
-			if cleanupErr := cleanupUprobeEntries(ids, openedFiles); cleanupErr != nil {
+			if cleanupErr := cleanupUprobeEntries(ids, openedFiles, false); cleanupErr != nil {
 				retErr = errors.Join(retErr, cleanupErr)
 			}
 		}
@@ -1003,7 +1006,7 @@ func createGenericUprobeSensor(
 		DisableNotAllowedReason: disableNotAllowedReason,
 		Statuses:                statuses,
 		DestroyHook: func() error {
-			return cleanupUprobeEntries(ids, openedFiles)
+			return cleanupUprobeEntries(ids, openedFiles, true)
 		},
 		PostLoadHook: func() error {
 			var errs error